2273389315fc29f885c3c898a95a4908e92854f7
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-14 19:00:41 -0500

    Randomly generates nodes each click

--- sketch.js
+++ sketch.js
@@ -1,10 +1,41 @@
 /// <reference path="./libraries/p5.d.ts" />
 
+var chance = 0.4;
+var length = 40;
+var size = 30;
+var maxDepth = 4;
+
 function setup() {
-  createCanvas(400, 400);
-  
+  createCanvas(600, 600);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+  drawImage();
 }
 
-function draw() {
-  background(220);
+function mouseClicked() {
+  drawImage();
+}
+
+function drawImage() {
+  background("#19224f");
+  fill(255);
+  drawNode(width/2,height/2,maxDepth);
+}
+
+function drawNode(x,y,depth) {
+  ellipse(x,y,50,50);
+  fill(255);
+  noStroke();
+  if(depth > 0) {
+    for(var dx = -1; dx <= 1; dx++) {
+      for(var dy = -1; dy <= 1; dy++) {
+        if(dx == 0 ^ dy == 0) {
+          if(Math.random() < chance) {
+            rect(x + dx * length, y + dy * length,dx * length * 2 + 10, dy * length * 2 + 10);
+            drawNode(x + dx * length * 2, y + dy * length * 2, depth - 1);
+          }
+        }
+      }
+    }
+  }
 }
