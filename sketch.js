6511f63205c464efbbe7423f9a5050920ff35f9a
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-14 19:07:09 -0500

    Nodes are smaller, color varies

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 /// <reference path="./libraries/p5.d.ts" />
 
-var chance = 0.4;
-var length = 40;
-var size = 30;
-var maxDepth = 4;
+var chance = 1;
+var length = 20;
+var size = 20;
+var maxDepth = 10;
 
 function setup() {
   createCanvas(600, 600);
@@ -23,15 +23,16 @@ function drawImage() {
 }
 
 function drawNode(x,y,depth) {
-  ellipse(x,y,50,50);
-  fill(255);
+  ellipse(x,y,size,size);
+  
   noStroke();
   if(depth > 0) {
     for(var dx = -1; dx <= 1; dx++) {
       for(var dy = -1; dy <= 1; dy++) {
         if(dx == 0 ^ dy == 0) {
-          if(Math.random() < chance) {
-            rect(x + dx * length, y + dy * length,dx * length * 2 + 10, dy * length * 2 + 10);
+          if(Math.random() < chance * depth / maxDepth) {
+            fill(depth / maxDepth * 255);
+            rect(x + dx * length, y + dy * length,dx * length * 2 + size/3, dy * length * 2 + size/3);
             drawNode(x + dx * length * 2, y + dy * length * 2, depth - 1);
           }
         }
