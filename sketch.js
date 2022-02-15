9935e5595d9c8b6f7bf9d58260a1440b6f24cd31
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-14 19:14:20 -0500

    Modified style a bit

--- sketch.js
+++ sketch.js
@@ -23,16 +23,16 @@ function mouseClicked() {
 function drawImage() {
   //Fills background
   background("#19224f");
+  noStroke();
   drawNode(width/2,height/2,maxDepth);
+  
 }
 
 function drawNode(x,y,depth) {
   
-  
-  noStroke();
+  fill((depth + 1) / maxDepth * 255);
+  ellipse(x,y,size * 2,size * 2);
   if(depth > 0) {
-    fill(depth / maxDepth * 255);
-    ellipse(x,y,size * 4/3,size * 4/3);
     for(var dx = -1; dx <= 1; dx++) {
       for(var dy = -1; dy <= 1; dy++) {
         if(dx == 0 ^ dy == 0) {
