d27971d1b4d2f941b47c28dface03d1e99a5a0b1
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-14 19:20:17 -0500

    Added comments

--- sketch.js
+++ sketch.js
@@ -29,16 +29,22 @@ function drawImage() {
 }
 
 function drawNode(x,y,depth) {
-  
+  //Create Node Ellipse
   fill((depth + 1) / maxDepth * 255);
   ellipse(x,y,size * 2,size * 2);
   if(depth > 0) {
+    //As long as the depth is positive, iterate between x = -1 to 1, and y = -1 to 1
     for(var dx = -1; dx <= 1; dx++) {
       for(var dy = -1; dy <= 1; dy++) {
+        //Only iterate if either dx == 0 or dy == 0
         if(dx == 0 ^ dy == 0) {
+          //Check if random is below the threshold. This threshold will reduce over-time
           if(Math.random() < chance * depth / maxDepth) {
+            //Update fill, do this so it keeps consistent
             fill(depth / maxDepth * 255);
+            //Draw the fconnecting rectangle
             rect(x + dx * length, y + dy * length,dx * length * 2 + size, dy * length * 2 + size);
+            //Recursively draw a new node at the end
             drawNode(x + dx * length * 2, y + dy * length * 2, depth - 1);
           }
         }
